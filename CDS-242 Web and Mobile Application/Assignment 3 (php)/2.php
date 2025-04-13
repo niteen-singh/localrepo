836cb998effb88d9dfd2eb49d678993a7d02cbb5
niteen-singh <viperxoffic@gmail.com>
2025-04-13 22:36:17 +0530

    updaated

--- CDS-242 Web and Mobile Application/Assignment 3 (php)/2.php
+++ CDS-242 Web and Mobile Application/Assignment 3 (php)/2.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Multiplication Table</title>
-</head>
-<body>
-
 <?php
-    $num1 = 10
-    echo "Multiplication Table of $num1 <br>";
-    
-    for ($i = 1; $i <= 10; $i++) {
-        echo "$num1 X $i = " . ($num1 * $i) . "<br>";
-    }    
+$num1 = 10;
+echo "Multiplication of $num1 <br>"; // Add a <br> to break line after the title
+for ($i = 1; $i <= 10; $i++) {
+    echo "$num1 X $i = " . ($num1 * $i) . "<br>"; // Add <br> here to break lines after each multiplication result
+}
 ?>
 
-</body>
-</html>
